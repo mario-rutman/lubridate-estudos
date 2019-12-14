7861bb894a9848277440b7098a3fc9dd16b16d27
mario-rutman <marrut@gmail.com>
2019-12-14 14:04:01 +0000

    commitando...

--- Estudando lubridate.R
+++ Estudando lubridate.R
@@ -1,6 +1,13 @@
 library(tidyverse)
 library(lubridate)
 
+# Para ficar no formato brasileiro. É fundamental para os dias e meses ficarem em português.
+Sys.setlocale("LC_ALL", "pt_BR.UTF-8")
+
+# Para não aparecer notação científica.
+options(scipen=999)
+
+
 # Há dois momentos: a leitura de um 'character' que será transformado em data; e, 
 # a leitura da data para ser transformada em texto normal de data, para ser lida.
  
@@ -20,7 +27,7 @@ today()
 # Data de hoje com as horas.
 now()
 
-# De uma data podemos esxtrair diversas informações.
+# De uma data podemos extrair diversas informações.
 mydate <- ymd("1988-09-29")
 
 ## extract year information
@@ -57,11 +64,13 @@ today() - dmy("29/9/2008") # Quantos dias tem Alice.
 today() - dmy("11/2/2006") # Quantos dias tem Anita.
 today() - dmy("30/03/1967") # Quantos dias tem Maria.
 today() - dmy("30/6/1959") # Quantos dias tem Mário.
-dmy("29/9/2018") - today() # Quantos dias faltam para o aniversário da Alice.
+dmy("29/9/2019") - today() # Quantos dias faltam para o aniversário da Alice.
 
 # as.duration dá a diferença entre datas em segundos, 
 # e aproxima para a unidade conveniente.  
 as.duration(today() - dmy("30/06/1959"))
+as.duration(today() - dmy("15/07/2019")) # tempo que falta para a viagem a Londres.
+
 
 as.duration(today() - ymd("1967-03-30"))
 as.duration(today() - dmy("30-3-1967"))
@@ -83,11 +92,11 @@ as.duration(dmy("24/11/1930") - dmy("30/6/1959")) # Idade do papai quando nasci.
 as.duration(dmy("29/9/2018") - today()) # Quantos segundos faltam para festa da alice.
 
 # Duration. Dá a duração em segundos de years, days, hours etc.
-dyears(x = 0.1)
-ddays(x = 15)
-ddays(x = 21)
-ddays(x = 2.5) 
-dhours(x = 0.1)
+dyears(x = 0.1) # quanto dura 0.1 anos?
+ddays(x = 15) # quanto dura 15 dias?
+ddays(x = 21) # quanto dura 21 dias?
+ddays(x = 2.5) # quanto dura 2.5 dias?
+dhours(x = 0.1) # quanto dura 0.1 horas?
 
 # Tem também uma forma mais completa.
 duration(0.1, unit = "years") 
@@ -99,11 +108,13 @@ dyears(x = 10.1) == duration(10.1, unit = "years")
 dhours(x = 0.1) == duration(0.1, unit = "hours")
 
 # Quanto falta para a viagem?
+as.duration(today() - dmy("14/07/2019")) # tempo que falta para a viagem a Londres.
 # Data da viagem menos now(), resultado em dias.
-round(ymd_hm("2018-07-17 21:05") - now(), digits = 1) 
+round(ymd_hm("2019-07-14 21:50") - now(), digits = 1) 
 
 # Resultado em segundos e outra unidade mais conveniente.
 round(as.duration(ymd_hm("2018-07-17 21:05") - now()), digits = 0)
+round(as.duration(ymd_hm("2019-07-14 21:05") - now()), digits = 0)
 
 
 # Esta é muito legal, você cria a função sf que 'depreende' 
@@ -135,6 +146,15 @@ year("1985-05-04") - year("1988-09-29")
 month("1985-05-04") - month("1988-09-29")
 day("1985-05-04") - day("1988-09-29")
 
-# dia da semana de uma data
-qwe <-wday(ymd('1985-05-04'), label = TRUE, abbr = FALSE)
-qwe
+# dia da semana de uma data. 
+# Como fiz o Sys.setlocale("LC_ALL", "pt_BR.UTF-8") o resultado é em português.
+wday(ymd('1985-05-04'), label = TRUE, abbr = FALSE)
+wday(ymd('1967-03-30'), label = TRUE, abbr = FALSE) # dia da semana que Maria nasceu.
+wday(ymd('2006-02-11'), label = TRUE, abbr = FALSE) # dia da semana que Anita nasceu.
+wday(ymd('2007-09-29'), label = TRUE, abbr = FALSE) # dia da semana que Alice nasceu.
+wday(ymd('1959-06-30'), label = TRUE, abbr = FALSE) # dia da semana que Mário nasceu.
+
+#
+#
+time_length(difftime(as.Date("2003-04-05"), as.Date("2001-01-01")), "years")
+time_length(difftime(Sys.time(), as.Date("2006-11-02")), "years")
